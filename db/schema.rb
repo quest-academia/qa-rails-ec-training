e3dee80c899fc575199d10934e959c9983a67361
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-10 16:52:36 +0900

    schema 作成

---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_12_10_072854) do
+
+  create_table "user_classifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "user_classification_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "password"
+    t.string "last_name"
+    t.string "first_name"
+    t.integer "zipcode"
+    t.string "prefecture"
+    t.string "municipality"
+    t.string "address"
+    t.string "apartments"
+    t.string "email"
+    t.string "phone_number"
+    t.bigint "user_classifications_id", null: false
+    t.string "company_name"
+    t.boolean "delete_flag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_classifications_id"], name: "index_users_on_user_classifications_id"
+  end
+
+  add_foreign_key "users", "user_classifications", column: "user_classifications_id"
+end

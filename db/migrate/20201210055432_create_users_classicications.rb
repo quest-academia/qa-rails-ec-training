137be67dab601eb3424c70780b18bec52f32e4af
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-10 16:49:46 +0900

    users_classicication 名称間違いのため削除

--- db/migrate/20201210055432_create_users_classicications.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersClassicications < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users_classicications do |t|
-      t.string :user_classification_name
-
-      t.timestamps
-    end
-  end
-end

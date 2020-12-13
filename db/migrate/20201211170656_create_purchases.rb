73474704eb0f3881f43958a30eb65089555403fe
git-ryou2405 <r2405.exe@gmail.com>
2020-12-13 11:13:43 +0900

    #100 typoの修正

--- db/migrate/20201211170656_create_purchases.rb
+++ db/migrate/20201211170656_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
       t.integer :purchase_price, null: false
-      t.integer :purchase_quanity, null: false
+      t.integer :purchase_quantity, null: false
       t.string :purchase_company, null: false
       t.datetime :order_date
       t.datetime :purchase_date

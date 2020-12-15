c606c294215331fde9b38360b68f70afdadba5cd
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-16 07:16:50 +0900

    t.string :password を直接消していた為復活

--- db/migrate/20201210072854_create_users.rb
+++ db/migrate/20201210072854_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :password
       t.string :last_name
       t.string :first_name
       t.integer :zipcode

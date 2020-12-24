6d189fe0099270ec99944db68ea20f77c26a66de
git-ryou2405 <r2405.exe@gmail.com>
2020-12-24 15:12:25 +0900

    新規登録後にsessionsにログイン情報を保持する

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @login_flag = false
   end
 
   def purchase_completed
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save!
-      session[:user_id] = @user.id
-      # log_in @user
+      log_in @user
       flash[:success] = "新規作成に成功しました。"
       redirect_to @user
     else

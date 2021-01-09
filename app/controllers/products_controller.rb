b13e970c4ed2e4b1761a5612d6037ac4a6370313
git-ryou2405 <r2405.exe@gmail.com>
2021-01-09 12:23:02 +0900

    検索ボタン押下時のみ、検索処理を開始

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,8 @@ class ProductsController < ApplicationController
   def index
     # 商品登録しているカテゴリのみ取得
     @categories = Category.joins(:products).where.not("products.category_id" => nil).distinct
+    return unless params[:search_button]
+
     if params[:search].present?
       @products = Product.search(params[:search], params[:id]).page(params[:page]).per(PER)
       flash.now[:notice] = "商品が見つかりません。" if @products.blank?

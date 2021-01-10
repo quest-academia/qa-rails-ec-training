0844f87d38c820f2597f6869b8b8c555fe5cad97
git-ryou2405 <r2405.exe@gmail.com>
2021-01-10 14:31:31 +0900

    商品検索結果表示で起きるN+1問題の解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
 
     if params[:search_product].present?
       @products = Product.search(params[:search_product], params[:category_id]).page(params[:page]).per(PER)
+      @category_name = Category.find(params[:category_id]).category_name
       flash.now[:notice] = "商品が見つかりません。" if @products.blank?
     else
       flash.now[:notice] = "商品名を入力してください"

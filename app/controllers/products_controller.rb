5109db84a80084967d9d454a42d08ca08688db9a
万壽本大嗣 <manju.manju.03.28@gmail.com>
2021-01-12 21:07:49 +0900

    商品詳細画面実装

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  # PER = 15
+  # def index
+  #   # 商品登録しているカテゴリのみ取得
+  #   @categories = Category.joins(:products).where.not("products.category_id" => nil).distinct
+  #   return unless params[:search_button]
+
+  #   if params[:search_product].present?
+  #     @products = Product.search(params[:search_product], params[:category_id]).page(params[:page]).per(PER)
+  #     @category_name = Category.find(params[:category_id]).category_name
+  #     flash.now[:notice] = "商品が見つかりません。" if @products.blank?
+  #   else
+  #     flash.now[:notice] = "商品名を入力してください"
+  #   end
+  # end
+
+  def show
+    # 商品を探してくる
+    @product = Product.find_by(id:params[:id])
+    # binding.pry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/purchase_completed', to: 'static_pages#purchase_completed', as: 'purchase_completed'  
+  get '/purchase_completed', to: 'static_pages#purchase_completed', as: 'purchase_completed'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
+  resources :products
 end

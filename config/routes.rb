f0e14a85ab0ad5ea7f6c0147ad318bcbfa9a617f
nishi622 <nishiiiiiii622@gmil.com>
2021-01-12 15:00:36 +0900

    sessionへのルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/purchase_completed', to: 'static_pages#purchase_completed', as: 'purchase_completed'  
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/signin', to: 'sessions#new'
   resources :users
 end

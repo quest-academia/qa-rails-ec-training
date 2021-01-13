0b375e7a2d913b6f619576bf26e5f8f126793a6f
nishi622 <nishiiiiiii622@gmil.com>
2021-01-13 10:45:48 +0900

    レビュー修正（signinをloginに変更）

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/purchase_completed', to: 'static_pages#purchase_completed', as: 'purchase_completed'  
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get '/signin', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
   resources :users
 end

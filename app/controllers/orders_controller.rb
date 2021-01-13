db9fdb8f09bd6d4ed671bb9c9a02d62c11608b65
miyukihashimoto <71243619+miyukihashimoto@users.noreply.github.com>
2021-01-13 17:30:46 +0900

    合計・注文キャンセルボタンの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,19 @@
 class OrdersController < ApplicationController
+  include OrdersHelper
+
   def show
     @order = Order.find_by(id: params[:id])
+  
+    cancel_button_indicate
+
+    @total = 0
+      @order.order_details.each do | order_detail | 
+        @total += order_detail.product.price * order_detail.order_quantity
+      end
+
     if @order.order_details.blank?
       render "blank"
     end
   end
+  
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,11 @@
 module OrdersHelper
+
+  def cancel_button_indicate #「注文をキャンセルする」ボタンは注文状態に「準備中」のものがあれば表示
+
+    @array = []
+      @order.order_details.each do | order_detail | 
+        @array = order_detail.shipment_status.shipment_status_name
+      end
+      
+  end
 end

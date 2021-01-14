01158f55d83d9c2e802700601efceafc3fef595e
nishi622 <nishiiiiiii622@gmil.com>
2021-01-14 18:25:57 +0900

    ログイン・ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash[:danger] = 'メールアドレスとパスワードが正しくありません' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    flash[:danger] = 'ログアウトしました'
+    redirect_to root_url
+  end
+  
 end

231d3cd83442c4c9e2197375c7d89bc69fb981aa
miyukihashimoto <71243619+miyukihashimoto@users.noreply.github.com>
2021-01-15 10:20:44 +0900

    指摘箇所修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: params[:id])
-
-    @total = @order.order_details.sum do | order_detail | 
-        order_detail.product.price * order_detail.order_quantity
-      end
-
-    if @order.order_details.blank?
-      render "_blank"
-    end
   end
   
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,10 +1,8 @@
 module OrdersHelper
 
   def cancel_button_indicate #「注文をキャンセルする」ボタンは注文状態に「準備中」のものがあれば表示
-    array = []
-      @order.order_details.each do | order_detail | 
-        array << order_detail.shipment_status.shipment_status_name
-      end
+
+    array = @order.order_details.map { | order_detail | order_detail.shipment_status.shipment_status_name }
 
     if array.include?("準備中")
       link_to "注文をキャンセルする","#", class: "btn btn-danger"

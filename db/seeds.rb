b43fe687fb19aeb7e37f8b72726839ce6174c8c7
miyukihashimoto <71243619+miyukihashimoto@users.noreply.github.com>
2021-01-15 13:47:00 +0900

    コンフリクト解消の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,6 +105,12 @@ end
 puts "User2 - Product & Purchase [#{Product.where(user_id: 2).count}data] Create!" # rubocop:disable Rails/Output
 
 # 発送状態
+shipment_status = ShipmentStatus.create!(shipment_status_name: "入荷待ち")
+shipment_status2 = ShipmentStatus.create!(shipment_status_name: "準備中")
+
+# ユーザー1が持つ注文
+user1 = User.first
+user1.orders.create!(order_date: Time.now)
 ShipmentStatus.create!(shipment_status_name: "入荷待ち")
 puts "ShipmentStatus Create!" # rubocop:disable Rails/Output
 
@@ -129,8 +135,22 @@ order1 = Order.first
   shipment_status_id: 1,
   order_detail_number: "foobar123",
   order_quantity: 1,
-  shipment_date: Time.current,
-)
+  shipment_date: Time.now
+  )
+  order1.order_details.create!(
+    product_id: 1,
+    shipment_status_id: 1,
+    order_detail_number: "foobar999",
+    order_quantity: 3,
+    shipment_date: Time.now
+    )
+  order1.order_details.create!(
+  product_id: 2,
+  shipment_status_id: 2,
+  order_detail_number: "foobar777",
+  order_quantity: 2,
+  shipment_date: Time.now
+  )
 
 # オーダー2が持つ注文の詳細
 order2 = Order.second

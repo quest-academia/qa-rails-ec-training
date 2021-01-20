d19a32dadbd1048fe3087f5c3a43c3f0c0319292
万壽本大嗣 <manju.manju.03.28@gmail.com>
2021-01-20 11:57:48 +0900

    ログインユーザーに紐づく注文

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,6 @@ class OrdersController < ApplicationController
   before_action :correct_user, only: [:show]
 
   def show
-    @order = Order.find_by(id: params[:id])
+    @order = Order.find_by(id: current_user[:id])
   end
 end

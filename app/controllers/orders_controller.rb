16ccd6be7b3a155e5c31b8d497af9f1838623d7a
万壽本大嗣 <manju.manju.03.28@gmail.com>
2021-01-20 13:16:18 +0900

    orders#show 修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,6 @@ class OrdersController < ApplicationController
   before_action :correct_user, only: [:show]
 
   def show
-    @order = Order.find_by(id: current_user[:id])
+    @order = current_user.orders.find_by(id: params[:id])
   end
 end

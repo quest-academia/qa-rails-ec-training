0ec3474910c6d807be86e0fa41164a6cf37a4272
Yuki.tachi <y.tachi0219@gmail.com>
2021-01-23 16:20:30 +0900

    商品名が空欄の時は商品全表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,8 @@ class ProductsController < ApplicationController
     @categories = Category.joins(:products).where.not("products.category_id" => nil).distinct
     return unless params[:search_button]
 
-    if params[:search_product_name].present?
-      @products = Product.includes(:category).search(params[:search_product_name], params[:category_id]).page(params[:page]).per(PER)
-      flash.now[:notice] = "商品が見つかりません。" if @products.blank?
-    else
-      flash.now[:notice] = "商品名を入力してください"
-    end
+    @products = Product.includes(:category).search(params[:search_product_name], params[:category_id]).page(params[:page]).per(PER)
+    flash.now[:notice] = "商品が見つかりません。" if @products.blank?
   end
 
   def show
